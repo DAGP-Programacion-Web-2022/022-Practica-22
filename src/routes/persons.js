41e18d44363614a3bd737ccd1b7ab850cbda96b2
dagp-ucol <dgentner@ucol.mx>
2022-05-29 18:22:02 -0500

    ✅ Practica 22 Finished

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -16,6 +16,34 @@ router.get('/persons', (req, res) => {
     });
 });
 
+// Delete Person
+router.get('/deletePerson/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id, req.body, (err, post) => {
+    });
+    res.redirect(req.get('referer'));
+});
+
+// SHows Person that is gonna be edited.
+router.get('/findById/:id', (req, res, next) => {
+    Person.findById(req.params.id, (err, person) => {
+        res.render('personUpdate', {'person' : person});
+    });
+});
+
+// Edit the contact
+router.post("/updatePerson", (req, res, next) => {
+    Person.findByIdAndUpdate(req.body.objId, {
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss }, (err, post) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("./persons");
+    });
+});
+
 // This function create and assign a route that points to a person site.
 router.get('/person', (req, res) => {
     res.render("person");
@@ -25,7 +53,7 @@ router.get('/', (req, res) => {
     res.render("index");
 });
 
-
+// This function create and assign a route that points to a person site.
 router.post("/addPerson", (req, res) => {
     const myPerson = new Person({
         nombre: req.body.nombre,
